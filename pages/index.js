4dc678231ddae9b7be03b7df75edfc9fa0d067d8
NatalNW7 <natanaelweslley1@gmail.com>
2024-03-07 20:55:57 -0300

    adicionando scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,9 @@
-function Home(){
-    return <h1>Vc é a minha princesa, a minha mulher, a minha musa e eu te amo muito!</h1>
+function Home() {
+  return (
+    <h1>
+      Vc é a minha princesa, a minha mulher, a minha musa e eu te amo muito!
+    </h1>
+  );
 }
 
 export default Home;
